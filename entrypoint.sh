37751b5db945c675f8818cc1b65c7ce80c03363d
xxaxxelxx <x@axxel.net>
2015-10-09 14:42:38 +0200

    port experiments

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,13 @@ case $1 in
 ;;
 esac
 
+# mode section
 if [ $MODE = "PROXY" ]; then
     cp -f /etc/icecast2/icecast_proxy.xml /etc/icecast2/icecast.xml
 elif [ $MODE = "PLAYER" ]; then
     cp -f /etc/icecast2/icecast_player.xml /etc/icecast2/icecast.xml
 fi
-
+# password section
 if [ -n "$IC_SOURCE_PASS" ]; then
     sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$IC_SOURCE_PASS<\/source-password>/g" /etc/icecast2/icecast.xml
 fi
@@ -32,7 +33,7 @@ fi
 if [ -n "$IC_ADMIN_PASS" ]; then
     sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$IC_ADMIN_PASS<\/admin-password>/g" /etc/icecast2/icecast.xml
 fi
-
+# address section
 if [ -n "$SIMULCAST_MASTER_SERVER" ]; then
     sed -i "s/<server>SIMULCAST_MASTER_SERVER<\/server>/<server>$SIMULCAST_MASTER_SERVER<\/server>/g" /etc/icecast2/icecast.xml
 fi
@@ -40,6 +41,7 @@ if [ -n "$CHANNEL_MASTER_SERVER" ]; then
     sed -i "s/<server>CHANNEL_MASTER_SERVER<\/server>/<server>$CHANNEL_MASTER_SERVER<\/server>/g" /etc/icecast2/icecast.xml
 fi
 
+# execution
 if [ $MODE = "BASH" ]; then
     bash
 else
